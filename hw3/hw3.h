f8c6bd2c06a3ed396eeb919395d17dd13436d060
Jonathon Nie <jonathonnie@Jonathons-MacBook-Pro.local>
2022-11-03 19:17:16 -0500

    hw3 directory and files

---
+++ hw3/hw3.h
@@ -0,0 +1,17 @@
+int all_positive(int a[], unsigned int alen);
+
+int exists_positive(int a[], unsigned int alen);
+
+int first_positive(int a[], unsigned int alen);
+
+unsigned int number_positives(int a[], unsigned int alen);
+
+void negate(int a[], unsigned int alen);
+
+int* partial_sums(int a[], unsigned int alen);
+
+void reverse(int a[], unsigned int alen);
+
+int* merge(int* a, unsigned int alen, int* b, unsigned int blen);
+
+unsigned int* primes_in_range(unsigned int lb, unsigned int ub, unsigned int* len);
